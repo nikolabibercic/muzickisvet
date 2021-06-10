8d6994a667d477e6b45cdf37e30c9dc3e3c7bcf1
nikolabibercic <nikolabibercic@gmail.com>
2021-06-10 22:37:14 +0200

    kreirani upiti za bazu

--- sql-queries.sql
+++ sql-queries.sql
@@ -1,4 +1,4 @@
-create table users(
+CREATE TABLE users(
 	user_id int AUTO_INCREMENT PRIMARY KEY,
 	name varchar(100) character set utf8 not null,
     email varchar(100) character set utf8 not null unique,
@@ -20,10 +20,71 @@ CREATE TABLE user_role(
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
 	FOREIGN KEY (role_id) REFERENCES sf_role(role_id)
 );
+
+CREATE TABLE sf_ad_category(
+	ad_category_id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(50) character set utf8 not null unique
+);
+
+CREATE TABLE sf_ad_type(
+	ad_type_id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(50) character set utf8 not null unique
+);
+
+CREATE TABLE sf_tag(
+	tag_id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(50) character set utf8 not null unique
+);
+
+CREATE TABLE ads(
+	ad_id int AUTO_INCREMENT PRIMARY KEY,
+	title varchar(50) character set utf8 not null,
+	text varchar(300) character set utf8 not null,
+	country varchar(100) character set utf8 not null,
+    city varchar(100) character set utf8 not null,
+	ad_category_id int not null,
+	ad_type_id int not null,
+	price int not null,
+	seen int not null,
+	FOREIGN KEY (ad_category_id) REFERENCES sf_ad_category(ad_category_id),
+	FOREIGN KEY (ad_type_id) REFERENCES sf_ad_type(ad_type_id)
+);
+
+CREATE TABLE ad_tag(
+	ad_tag_id int AUTO_INCREMENT PRIMARY KEY,
+	ad_id int not null,
+	tag_id int not null,
+	FOREIGN KEY (ad_id) REFERENCES ads(ad_id),
+	FOREIGN KEY (tag_id) REFERENCES sf_tag(tag_id)
+);
 -------------------------------------------------------------------------------------------------
 ---OBAVEZNI INSERTI
 -------------------------------------------------------------------------------------------------
 insert into sf_role values(null,'Admin');
+
+insert into sf_ad_category values(null,'Vokal');
+insert into sf_ad_category values(null,'Gitara');
+insert into sf_ad_category values(null,'Bas gitara');
+insert into sf_ad_category values(null,'Klavijatura');
+insert into sf_ad_category values(null,'Harmonika');
+insert into sf_ad_category values(null,'Bubnjevi');
+insert into sf_ad_category values(null,'Gudački instrumenti');
+insert into sf_ad_category values(null,'Duvački instrumenti');
+insert into sf_ad_category values(null,'Perkusije');
+insert into sf_ad_category values(null,'Bend');
+
+insert into sf_ad_type values(null,'Standard');
+insert into sf_ad_type values(null,'Premium');
+
+insert into sf_tag values(null,'Pop');
+insert into sf_tag values(null,'Rock');
+insert into sf_tag values(null,'Folk');
+insert into sf_tag values(null,'Jazz');
+insert into sf_tag values(null,'Metal');
+insert into sf_tag values(null,'Rhythm & blues (R&B)');
+insert into sf_tag values(null,'Rap/Hip-hop');
+insert into sf_tag values(null,'Funk/Soul');
+insert into sf_tag values(null,'Electronic');
 -------------------------------------------------------------------------------------------------
 ---OPCIONI INSERTI
 -------------------------------------------------------------------------------------------------
