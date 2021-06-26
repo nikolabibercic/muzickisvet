6b7f2bfcd415ea9ff58185d6ec49d305ed85faf5
nikolabibercic <nikolabibercic@gmail.com>
2021-06-26 12:17:14 +0200

    dorada footera

--- partials/footer.php
+++ partials/footer.php
@@ -1,19 +1,19 @@
     <footer>
-        <article>
-            <div class="icons">
-                <a href=""><i class="fab fa-facebook-square"></i></a>
-                <a href=""><i class="fab fa-instagram"></i></a>
-                <a href=""><i class="fab fa-twitter"></i></a>
-                <a href=""><i class="fab fa-youtube"></i></a>
-            </div>
-        </article>
-        <article>
-            <ul class="footerUl">
+        <section class="footerSection1 container2">
+            <ul>
+                <li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                <li><a href=""><i class="fab fa-youtube"></i></a></li>
+            </ul>
+        </section>
+        <section class=" footerSection2 container2">
+            <ul>
                 <li><a href="view-contact.php">Uslovi oglašavanja</a></li>
                 <li><a href="view-contact.php">Kontakt</a></li>
             </ul>
             <p>MuzickiSvet.rs &copy; All rights reserved.</p>
-        </article>
+        </section>
     </footer>
 
     <script src="main.js"></script> 
